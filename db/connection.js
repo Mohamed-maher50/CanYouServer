fb1d79cad99bbdf254372701901c6665c718ba80
‪mohamed maher‬‏ <78468030+Mohamed-maher50@users.noreply.github.com>
2023-02-25 22:40:39 +0200

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.ATLSURL, (err) => {
+mongoose.connect(process.env.ATLSURL,{ useNewUrlParser: true } ,(err) => {
   if (err) console.log(err);
   console.log("connect success");
 });
