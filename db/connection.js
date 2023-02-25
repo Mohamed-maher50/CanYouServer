9b0661d2b85a940fe307767f0d658b52179c98ab
mohamed maher <mohamedmaher.mm330@gmail.com>
2023-02-25 21:28:21 +0200

    upload server

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_CONNECTION_URL, (err) => {
+mongoose.connect(process.env.ATLSURL, (err) => {
   if (err) console.log(err);
   console.log("connect success");
 });
+// fK31oBGl445hbuIM
