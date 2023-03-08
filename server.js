e80f9065593e6e9b4c311c5c1d7fbb7d1bbbc3dc
‪mohamed maher‬‏ <78468030+Mohamed-maher50@users.noreply.github.com>
2023-03-08 22:39:04 +0200

    Update server.js
    sdf

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 1000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
   },
 });
 module.exports = {
